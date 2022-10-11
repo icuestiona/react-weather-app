d50cf12cdaea64104e9019993d476f4101069912
Soraya <icuestiona@gmail.com>
2022-10-11 11:39:53 +0200

    Modified WeatherForecast function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,33 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = `ec906dafd44a254d26b9dd410c431070`;
-  let longitude = props.coordinate.lon;
-  let latitude = props.coordinate.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Tue</div>
-          <WeatherIcon code="01d" size={64} />
-          <div className="WeatherForecast-temps">
-            <span className="WeatherForecast-temp-max">19º</span>
-            <span className="WeatherForecast-temp-min">9º</span>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `ec906dafd44a254d26b9dd410c431070`;
+    let longitude = props.coordinate.lon;
+    let latitude = props.coordinate.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
