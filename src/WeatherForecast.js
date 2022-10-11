6c21662db7fc1e76f76c7e88e2d3363e91f656c3
Soraya <icuestiona@gmail.com>
2022-10-11 13:34:00 +0200

    Modified return

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -29,5 +29,7 @@ export default function WeatherForecast(props) {
     let latitude = props.coordinate.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+
+    return null;
   }
 }
